547e6d2d8da3fb0931c23c7bf1f6288861622ed5
William Torres <william.dos.santos@prservicos.com.br>
2021-12-30 07:18:36 -0400

    [update] start learn objective-c

---
+++ ObjcetivecEssentitalTraining/ObjcetivecEssentitalTraining/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcetivecEssentitalTraining
+//
+//  Created by William Torres Dias dos Santos (P) on 30/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
